0bef63ecaa99ce087662d65a2daa0ba7273d1d77
Senott <senott@yahoo.com>
2020-10-21 10:41:40 -0300

    Get user position from browser.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 const App: React.FC = () => {
-  console.log(process.env.REACT_APP_GOOGLE_API_KEY);
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+  async function setCurrentLocation() {
+    await navigator.geolocation.getCurrentPosition(
+      position => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      },
+      error => {
+        alert('Sem a sua localização este app é inútil.');
+      },
+    );
+  }
+
+  useEffect(() => {
+    setCurrentLocation();
+  }, []);
+
   return (
     <>
       <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY!}>
         <GoogleMap
           mapContainerStyle={{ height: '100vh', width: '100vw' }}
           zoom={15}
-          center={{ lat: -21.7768606, lng: -41.3109657 }}
+          center={{ lat: latitude, lng: longitude }}
         ></GoogleMap>
       </LoadScript>
     </>
