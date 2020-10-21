b9d5bce116c33a6407f9f652d2671ba4a2f39bdc
Senott <senott@yahoo.com>
2020-10-21 10:25:12 -0300

    Project configuration and first map visualization.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 const App: React.FC = () => {
-  return <h1>Hello World</h1>;
-}
+  console.log(process.env.REACT_APP_GOOGLE_API_KEY);
+  return (
+    <>
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY!}>
+        <GoogleMap
+          mapContainerStyle={{ height: '100vh', width: '100vw' }}
+          zoom={15}
+          center={{ lat: -21.7768606, lng: -41.3109657 }}
+        ></GoogleMap>
+      </LoadScript>
+    </>
+  );
+};
 
 export default App;
